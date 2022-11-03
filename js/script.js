4e3ae9fd631b6db4ad8c08e830eed1dfb033ff18
leonardo-marcolino <leo.marcolino14@gmail.com>
2022-11-02 21:37:04 -0300

    first_commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const botao =  document.getElementById("btn-mobile");
+botao.addEventListener("click",togglemuno);
+
+function togglemuno(){
+    const nav = document.getElementById("nav")
+    nav.classList.toggle('active')
+    
+
+    
+}
